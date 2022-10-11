7a3f627bf5664bef86938cbe9b34bc6bbdc77514
Ariel Malka <ariel@chronotext.org>
2022-10-11 21:12:45 +0300

    Drawin3dText in place

---
+++ sketches/Drawing3dText/src/Sketch.cpp
@@ -0,0 +1,113 @@
+#include "Sketch.h"
+
+using namespace std;
+using namespace chr;
+using namespace gl;
+
+static constexpr float FOVY = 45;
+static constexpr float CAMERA_DISTANCE = 180;
+static constexpr float CAMERA_ELEVATION = -85;
+
+static constexpr float R = 50;
+static constexpr float TURNS = 10;
+static constexpr float H = 100;
+
+void Sketch::setup()
+{
+    auto lines = utils::readLines<u16string>(InputSource::resource("song.txt")); // Lyrics by Genesis
+
+    u16string text;
+    for (const auto &line : lines)
+    {
+        text += line + u" ";
+    }
+
+    font = fontManager.getFont(InputSource::resource("Helvetica_Bold_64.fnt"), XFont::Properties3d());
+    font->setShader(textureAlphaShader);
+    font->setSize(8);
+
+    float L = TWO_PI * TURNS * R;
+    float offset = (L - font->getStringAdvance(text)) / 2;
+
+    font->beginSequence(sequence);
+    drawTextHelix(*font, text, R, TURNS, -H, offset, font->getOffsetY(XFont::ALIGN_MIDDLE));
+    font->endSequence();
+
+    // ---
+
+    glDisable(GL_DEPTH_TEST);
+    glDepthMask(GL_FALSE);
+    glEnable(GL_CULL_FACE);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void Sketch::resize()
+{
+    camera
+        .setFov(FOVY)
+        .setClip(1, 1000)
+        .setWindowSize(windowInfo.size);
+}
+
+void Sketch::draw()
+{
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // ---
+
+    camera.getViewMatrix()
+        .setIdentity()
+        .scale(1, -1, 1)
+        .translate(0, 0, -CAMERA_DISTANCE)
+        .rotateX(-CAMERA_ELEVATION * D2R)
+        .rotateZ(clock()->getTime() * 0.5f)
+        .translate(0, 0, H / 2);
+
+    State()
+        .setShaderMatrix(camera.getViewProjectionMatrix())
+        .apply();
+
+    // ---
+
+    glCullFace(GL_FRONT);
+    font->setColor(1, 1, 1, 0.333f);
+    font->replaySequence(sequence);
+
+    glCullFace(GL_BACK);
+    font->setColor(1, 1, 1, 1);
+    font->replaySequence(sequence);
+}
+
+void Sketch::drawTextHelix(XFont &font, const u16string &text, float r, float turns, float h, float offsetX, float offsetY)
+{
+    float l = TWO_PI * turns;
+    float L = PI * turns * (r + r);
+    float dz = h / l;
+    float ay = -atan2f(h, L);
+
+    Matrix matrix;
+
+    for (auto c : text)
+    {
+        auto glyphIndex = font.getGlyphIndex(c);
+        float halfWidth = font.getGlyphAdvance(glyphIndex) / 2;
+        offsetX += halfWidth;
+
+        if (glyphIndex >= 0)
+        {
+            float d = offsetX / r;
+            matrix
+                .setTranslate(-cosf(d) * r, +sinf(d) * r, d * dz)
+                .rotateZ(HALF_PI - d)
+                .rotateY(ay)
+                .rotateX(-HALF_PI);
+
+            font.addGlyph(matrix, glyphIndex, -halfWidth, offsetY);
+        }
+
+        offsetX += halfWidth;
+    }
+}
---
+++ sketches/Drawing3dText/src/Sketch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "chr/cross/Context.h"
+#include "chr/gl/Batch.h"
+#include "chr/gl/Camera.h"
+#include "chr/gl/shaders/TextureAlphaShader.h"
+
+#include "common/xf/FontManager.h"
+
+class Sketch : public chr::CrossSketch
+{
+public:
+    void setup() final;
+    void resize() final;
+    void draw() final;
+
+protected:
+    chr::gl::Camera camera;
+
+    chr::xf::FontManager fontManager;
+    std::shared_ptr<chr::xf::Font> font;
+    chr::xf::FontSequence sequence;
+    chr::gl::shaders::TextureAlphaShader textureAlphaShader;
+
+    static void drawTextHelix(chr::XFont &font, const std::u16string &text, float r, float turns, float h, float offsetX = 0, float offsetY = 0);
+};
